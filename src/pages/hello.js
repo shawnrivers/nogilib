a9823d9371c2facd78b7f577ff700a846cff9c19
Shawn Rivers <kaushou@gmail.com>
2019-07-04 12:31:37 +0900

    Split hello component out of index.js

---
+++ src/pages/hello.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Hello = () => <div>Hello, world!</div>;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
-import React from "react"
+import React from 'react';
+import { Hello } from './hello';
 
-export default () => <div>Hello world!</div>
+export default Hello;
