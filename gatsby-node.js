5b53ccafc12f149874754812a968752f61372045
Shawn Rivers <kaushou@gmail.com>
2019-08-13 18:13:54 +0900

    Create pages for each locale

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,26 +1,26 @@
 /* eslint-disable */
 const { createFilePath } = require(`gatsby-source-filesystem`);
 const path = require(`path`);
+const locales = require(`./src/i18n/locales`);
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
 
   if (node.internal.type === "AlbumsJson") {
-    console.log(node.number, node.title);
-    // const slug = createFilePath({ node, getNode, basePath: "pages" });
+    const slug = createFilePath({ node, getNode, basePath: "pages" });
     createNodeField({
       node,
       name: "slug",
-      value: "/albums/" + node.number,
+      value: slug + node.number,
     });
   }
 
   if (node.internal.type === "SinglesJson") {
-    console.log(node.number, node.title);
+    const slug = createFilePath({ node, getNode, basePath: "pages" });
     createNodeField({
       node,
       name: "slug",
-      value: "/singles/" + node.number,
+      value: slug + node.number,
     });
   }
 };
@@ -60,31 +60,70 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return;
   }
 
+  const localesKeys = Object.keys(locales);
+
   albumsResult.data.allAlbumsJson.edges.forEach(({ node }) => {
     const cdType = node.fields.slug.split("/")[1];
     const number = node.fields.slug.split("/")[2];
 
-    createPage({
-      path: node.fields.slug,
-      component: path.resolve("./src/components/templates/Album/index.tsx"),
-      context: {
-        number,
-        cdType,
-      },
-    });
+    for (const lang of localesKeys) {
+      const localizedPath = locales[lang].default
+        ? node.fields.slug
+        : locales[lang].path + node.fields.slug;
+
+      createPage({
+        path: localizedPath,
+        component: path.resolve("./src/components/templates/Album/index.tsx"),
+        context: {
+          number,
+          cdType,
+          locale: lang,
+        },
+      });
+    }
   });
 
   singlesResult.data.allSinglesJson.edges.forEach(({ node }) => {
     const cdType = node.fields.slug.split("/")[1];
     const number = node.fields.slug.split("/")[2];
 
-    createPage({
-      path: node.fields.slug,
-      component: path.resolve("./src/components/templates/Single/index.tsx"),
-      context: {
-        number,
-        cdType,
-      },
+    for (const lang of localesKeys) {
+      const localizedPath = locales[lang].default
+        ? node.fields.slug
+        : locales[lang].path + node.fields.slug;
+
+      createPage({
+        path: localizedPath,
+        component: path.resolve("./src/components/templates/Single/index.tsx"),
+        context: {
+          number,
+          cdType,
+        },
+      });
+    }
+  });
+};
+
+exports.onCreatePage = ({ page, actions }) => {
+  const { createPage, deletePage } = actions;
+
+  return new Promise(resolve => {
+    deletePage(page);
+
+    Object.keys(locales).map(lang => {
+      const localizedPath = locales[lang].default
+        ? page.path
+        : locales[lang].path + page.path;
+
+      return createPage({
+        ...page,
+        path: localizedPath,
+        context: {
+          locale: lang,
+        },
+      });
     });
+
+    resolve();
   });
 };
---
+++ src/i18n/locales.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+module.exports = {
+  en: {
+    path: "en",
+    local: "English",
+  },
+  ja: {
+    path: "ja",
+    local: "日本語",
+    default: true,
+  },
+  zh: {
+    path: "zh",
+    local: "中文",
+  },
+};
