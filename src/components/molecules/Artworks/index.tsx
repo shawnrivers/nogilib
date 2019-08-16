c0020f2ee2265f2e7b9451bfe231d967833601c3
Shawn Rivers <kaushou@gmail.com>
2019-08-16 17:18:43 +0900

    Adjust artworks animation

--- src/components/molecules/Artworks/index.tsx
+++ src/components/molecules/Artworks/index.tsx
@@ -16,16 +16,6 @@ interface ArtworksProps {
 export const Artworks = ({ artworks, title, className }: ArtworksProps) => {
   const [artworkIndex, setArtworkIndex] = React.useState(0);
 
-  const imageRef = React.useRef(null);
-
-  const imageWidth = React.useMemo(() => {
-    if (imageRef.current !== null) {
-      // @ts-ignore
-      return { width: imageRef.current.clientWidth, unit: "px" };
-    }
-    return { width: 50, unit: "vw" };
-  }, [imageRef.current]);
-
   const handleClickNextArtwork = React.useCallback(() => {
     if (artworkIndex < artworks.length - 1) {
       setArtworkIndex(artworkIndex + 1);
@@ -44,25 +34,21 @@ export const Artworks = ({ artworks, title, className }: ArtworksProps) => {
         <button onClick={handleClickPreviousArtwork}>
           <ArrowLeftIcon className={styles.arrow} />
         </button>
-        <div className={styles.images} ref={imageRef}>
-          <motion.div
-            animate={{
-              transform: `translateX(${artworkIndex * -imageWidth.width}${
-                imageWidth.unit
-              })`,
-            }}
-            className={styles.images_container}
-          >
-            {artworks.map((artwork, index) => (
-              <img
-                key={index}
-                src={artwork.medium}
-                srcSet={`${artwork.large} 1.5x`}
-                alt={title}
-                className={styles.image}
-              />
-            ))}
-          </motion.div>
+        <div className={styles.images}>
+          {artworks.map((artwork, index) => (
+            <motion.img
+              animate={
+                artworkIndex === index
+                  ? { opacity: 1, scale: 1 }
+                  : { opacity: 0, scale: 0.5 }
+              }
+              key={index}
+              src={artwork.medium}
+              srcSet={`${artwork.large} 1.5x`}
+              alt={title}
+              className={styles.image}
+            />
+          ))}
         </div>
         <button onClick={handleClickNextArtwork}>
           <ArrowRightIcon className={styles.arrow} />
@@ -71,13 +57,12 @@ export const Artworks = ({ artworks, title, className }: ArtworksProps) => {
       <div className={styles.indicator}>
         {artworks.map((_, index) => (
           <button key={index} onClick={() => setArtworkIndex(index)}>
-            <motion.div
-              animate={{
+            <div
+              style={{
                 backgroundColor: index === artworkIndex ? "#757575" : "#ffffff",
               }}
-              transition={{ duration: 0.2 }}
               className={styles.dot}
-            ></motion.div>
+            />
           </button>
         ))}
       </div>
