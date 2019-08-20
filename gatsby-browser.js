47cf306c0d3ff5c89a65a77b4ff7f2d2e05e01e4
Shawn Rivers <kaushou@gmail.com>
2019-08-21 00:38:51 +0900

    Add service worker

---
+++ gatsby-browser.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+  if (answer === true) {
+    window.location.reload();
+  }
+};
+
+export const registerServiceWorker = () => true;
--- gatsby-config.js
+++ gatsby-config.js
@@ -46,5 +46,6 @@ module.exports = {
         crossOrigin: `use-credentials`,
       },
     },
+    `gatsby-plugin-offline`,
   ],
 };
