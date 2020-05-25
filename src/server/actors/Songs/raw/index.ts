600f14f8d19a9615b595be56ce7fcfdbde43edcc
Shawn Rivers <kaushou@gmail.com>
2020-05-25 23:03:07 +0900

    Remove verbose files

--- src/server/actors/Songs/raw/index.ts
+++ src/server/actors/Songs/raw/index.ts
@@ -1,6 +1,6 @@
 import { SINGLE_SONGS } from "server/actors/Songs/raw/editor/singles";
 import { OTHER_SONGS } from "server/actors/Songs/raw/editor/others";
-import { ALBUM_SONGS } from "server/actors/Songs/raw/editor";
+import { ALBUM_SONGS } from "server/actors/Songs/raw/editor/albums";
 import { SongsRawArray } from "server/actors/Songs/models";
 
 export const songsRawArray: SongsRawArray = [
