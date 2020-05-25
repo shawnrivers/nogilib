f1578b9e828eeaf34664c2270eb7bbe66f3f2982
Shawn Rivers <kaushou@gmail.com>
2020-05-25 21:47:39 +0900

    Fix songs raw data directory

--- src/server/actors/Songs/raw/index.ts
+++ src/server/actors/Songs/raw/index.ts
@@ -1,6 +1,6 @@
-import { SINGLE_SONGS } from "server/actors/Songs/raw/albums/singles";
-import { OTHER_SONGS } from "server/actors/Songs/raw/albums/others";
-import { ALBUM_SONGS } from "server/actors/Songs/raw/albums";
+import { SINGLE_SONGS } from "server/actors/Songs/raw/editors/singles";
+import { OTHER_SONGS } from "server/actors/Songs/raw/editors/others";
+import { ALBUM_SONGS } from "server/actors/Songs/raw/editors";
 import { SongsRawArray } from "server/actors/Songs/models";
 
 export const songsRawArray: SongsRawArray = [
