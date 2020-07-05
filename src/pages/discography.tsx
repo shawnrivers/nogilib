5ab451bfc0ac5f04a33b1d9e284c20aefcce2336
Shawn Rivers <kaushou@gmail.com>
2020-07-05 18:36:33 +0900

    Fix discography basic layout and query actual data

--- src/pages/discography.tsx
+++ src/pages/discography.tsx
@@ -1,13 +1,58 @@
 /**@jsx jsx */
-import { jsx, css } from '@emotion/core';
-import styled from '@emotion/styled';
+import { css, jsx } from '@emotion/core';
+import { graphql } from 'gatsby';
 import * as React from 'react';
-import { Typography } from 'client/components/atoms/Typography';
+import styled from '@emotion/styled';
 import { Image } from 'client/components/atoms/Image';
+import { Typography } from 'client/components/atoms/Typography';
+import { CdResult } from 'server/actors/Cds/models';
+import { toCdNumber } from 'utils/strings';
+
+export const query = graphql`
+  query SinglesQuery {
+    allSinglesJson {
+      nodes {
+        title
+        number
+        artworks
+        release
+        songs {
+          focusPerformers {
+            name
+            type
+          }
+        }
+      }
+    }
+  }
+`;
+
+type FocusPerformers = CdResult['songs'][0]['focusPerformers'];
+
+type QueryResultCds = {
+  title: CdResult['title'];
+  number: CdResult['number'];
+  artworks: CdResult['artworks'];
+  release: CdResult['release'];
+  songs: {
+    focusPerformers: FocusPerformers;
+  }[];
+}[];
+
+type QueryResult = {
+  data: {
+    allSinglesJson: {
+      nodes: QueryResultCds;
+    };
+  };
+};
 
 const Container = styled.div`
   display: grid;
-  grid-template-columns: 200px auto 140px;
+  grid-template-columns: minmax(min-content, 200px) minmax(min-content, 1fr) minmax(
+      max-content,
+      140px
+    );
   grid-template-rows: auto;
   grid-template-areas:
     'navigation header settings'
@@ -29,8 +74,61 @@ const Settings = styled.div`
 
 const Main = styled.main`
   grid-area: main;
-  margin: auto;
 `;
+
+const FeatureCd: React.FC<{
+  artwork: string;
+  number: string;
+  title: string;
+  focusPerformers: FocusPerformers;
+  release: string;
+}> = props => (
+  <div
+    css={css`
+      display: flex;
+    `}
+  >
+    <ArtworkImage
+      src={props.artwork}
+      alt={props.number}
+      width={300}
+      height={300}
+    />
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+      `}
+    >
+      <Typography variant="h1" element="h4">
+        {toCdNumber(props.number)} <br /> Single
+      </Typography>
+      <div>
+        <Typography variant="h5">{props.title}</Typography>
+        <Typography variant="body1">
+          {props.focusPerformers.type}: {props.focusPerformers.name}
+        </Typography>
+        <Typography variant="body1">発売日：{props.release}</Typography>
+      </div>
+    </div>
+  </div>
+);
+
+const NormalCd: React.FC<{
+  artwork: string;
+  number: string;
+  title: string;
+  focusPerformers: FocusPerformers;
+}> = props => (
+  <div>
+    <ArtworkImage src={props.artwork} alt={props.number} />
+    <Typography variant="h4">{toCdNumber(props.number)}</Typography>
+    <Typography variant="body1">{props.title}</Typography>
+    <Typography variant="body2">
+      {props.focusPerformers.type}: {props.focusPerformers.name}
+    </Typography>
+  </div>
+);
 
 const ArtworkImage: React.FC<{
   src: string;
@@ -50,7 +148,50 @@ const ArtworkImage: React.FC<{
   />
 );
 
-const Discography: React.FC = () => {
+type CdGroupByYear = {
+  year: number;
+  cds: (QueryResultCds[0] & { year: number })[];
+};
+
+const groupCdsByYear = (cds: QueryResultCds): CdGroupByYear[] => {
+  const cdsWithYear = cds
+    .map(cd => ({
+      ...cd,
+      year: new Date(cd.release).getFullYear(),
+    }))
+    .sort((cdA, cdB) => cdB.year - cdA.year);
+
+  const cdGroupsByYear: CdGroupByYear[] = [];
+
+  for (const cd of cdsWithYear) {
+    if (cdGroupsByYear.length === 0) {
+      cdGroupsByYear.push({ year: cd.year, cds: [cd] });
+    } else {
+      for (let i = 0; i < cdGroupsByYear.length; i++) {
+        const cdGroup = cdGroupsByYear[i];
+        if (cdGroup.year === cd.year) {
+          cdGroup.cds.push(cd);
+          break;
+        } else if (i === cdGroupsByYear.length - 1) {
+          cdGroupsByYear.push({ year: cd.year, cds: [cd] });
+          break;
+        }
+      }
+    }
+  }
+
+  return cdGroupsByYear;
+};
+
+const Discography: React.FC<QueryResult> = props => {
+  const {
+    data: {
+      allSinglesJson: { nodes },
+    },
+  } = props;
+
+  const cdGroupsByYear = groupCdsByYear(nodes);
+
   return (
     <Container>
       <Navigation>
@@ -75,81 +216,100 @@ const Discography: React.FC = () => {
         </Typography>
       </Settings>
       <Header>
-        <Typography variant="h1">DISCOGRAPHY</Typography>
+        <Typography
+          variant="h1"
+          css={css`
+            overflow-wrap: break-word;
+          `}
+        >
+          DISCOGRAPHY
+        </Typography>
         <Typography variant="h2">Singles / Albums</Typography>
       </Header>
       <Main>
-        <Typography
-          variant="h3"
-          css={css`
-            text-align: center;
-          `}
-        >
-          2020
-        </Typography>
-        <div
-          css={css`
-            display: flex;
-          `}
-        >
-          <ArtworkImage
-            src="artworks/singles/25/A.jpg"
-            alt="25"
-            width={300}
-            height={300}
-          />
-          <div
-            css={css`
-              display: flex;
-              flex-direction: column;
-            `}
-          >
-            <Typography variant="h1" element="h4">
-              25th. <br /> Single
-            </Typography>
-            <div>
-              <Typography variant="h5">しあわせの保護色</Typography>
-              <Typography variant="body1">Center: 白石麻衣</Typography>
-              <Typography variant="body1">発売日：2020.3.25</Typography>
+        {cdGroupsByYear.map((cdGroup, i) => {
+          if (i === 0) {
+            const [featuredCd, ...restCds] = cdGroup.cds;
+
+            return (
+              <div key={cdGroup.year}>
+                <Typography
+                  variant="h3"
+                  css={css`
+                    text-align: center;
+                  `}
+                >
+                  {cdGroup.year}
+                </Typography>
+                <div
+                  css={css`
+                    display: grid;
+                    grid-template-columns: repeat(3, 200px);
+                    grid-template-rows: auto;
+                    grid-template-areas: 'featured featured featured';
+                    grid-gap: 24px;
+                    justify-content: center;
+                  `}
+                >
+                  <div
+                    css={css`
+                      grid-area: featured;
+                    `}
+                  >
+                    <FeatureCd
+                      artwork={featuredCd.artworks[0]}
+                      number={featuredCd.number}
+                      title={featuredCd.title}
+                      focusPerformers={featuredCd.songs[0].focusPerformers}
+                      release={featuredCd.release}
+                    />
+                  </div>
+                  {restCds.map(cd => (
+                    <NormalCd
+                      key={cd.number}
+                      artwork={cd.artworks[0]}
+                      number={cd.number}
+                      title={cd.title}
+                      focusPerformers={cd.songs[0].focusPerformers}
+                    />
+                  ))}
+                </div>
+              </div>
+            );
+          }
+
+          return (
+            <div key={cdGroup.year}>
+              <Typography
+                variant="h3"
+                css={css`
+                  text-align: center;
+                `}
+              >
+                {cdGroup.year}
+              </Typography>
+              <div
+                css={css`
+                  display: grid;
+                  grid-template-columns: repeat(3, 200px);
+                  grid-template-rows: auto;
+                  grid-gap: 24px;
+                  justify-content: center;
+                `}
+              >
+                {cdGroup.cds.map(cd => (
+                  <NormalCd
+                    key={cd.number}
+                    artwork={cd.artworks[0]}
+                    number={cd.number}
+                    title={cd.title}
+                    focusPerformers={cd.songs[0].focusPerformers}
+                  />
+                ))}
+              </div>
             </div>
-          </div>
-        </div>
-        <Typography
-          variant="h3"
-          css={css`
-            text-align: center;
-          `}
-        >
-          2019
-        </Typography>
-        <div
-          css={css`
-            display: grid;
-            grid-template-columns: repeat(auto-fill, 200px);
-            grid-gap: 24px;
-          `}
-        >
-          <div>
-            <ArtworkImage src="artworks/singles/24/A.jpg" alt="24" />
-            <Typography variant="h4">24th.</Typography>
-            <Typography variant="body1">
-              夜明けまで強がらなくてもいい
-            </Typography>
-            <Typography variant="body2">Center: 遠藤さくら</Typography>
-          </div>
-          <div>
-            <ArtworkImage src="artworks/singles/23/A.jpg" alt="23" />
-            <Typography variant="h4">23rd.</Typography>
-            <Typography variant="body1">Sing Out!</Typography>
-            <Typography variant="body2">Center: 齋藤飛鳥</Typography>
-          </div>
-          <div>
-            <ArtworkImage src="artworks/singles/22/A.jpg" alt="22" />
-            <Typography variant="h4">22nd.</Typography>
-            <Typography variant="body1">帰り道は遠回りしたくなる</Typography>
-            <Typography variant="body2">Center: 西野七瀬</Typography>
-          </div>
-        </div>
+          );
+        })}
       </Main>
     </Container>
   );
