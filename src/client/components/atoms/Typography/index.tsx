d1bd4c25b292dfbce43ba0b60446fb2daab0c368
Shawn Rivers <kaushou@gmail.com>
2020-07-24 23:58:30 +0900

    Move font family setting to Typography component

--- src/client/components/atoms/Typography/index.tsx
+++ src/client/components/atoms/Typography/index.tsx
@@ -2,7 +2,11 @@
 import { css, jsx } from '@emotion/core';
 import * as React from 'react';
 import { useTheme } from 'client/styles/tokens';
-import { TypographyKey } from 'client/styles/typography';
+import {
+  FontFamily,
+  FONT_FAMILY,
+  TypographyKey,
+} from 'client/styles/typography';
 import {
   ThemeColorsForeground,
   ThemeColorVariants,
@@ -27,10 +31,30 @@ const variantMapping: Record<TypographyKey, React.ElementType> = {
   button: 'span',
 };
 
+const defaultFontFamilyMapping: Record<TypographyKey, FontFamily> = {
+  h1: 'serif',
+  h2: 'serif',
+  h3: 'serif',
+  h4: 'serif',
+  h5: 'serif',
+  h6: 'serif',
+  h7: 'serif',
+  body1: 'sans',
+  body2: 'sans',
+  body3: 'sans',
+  body4: 'sans',
+  em1: 'sans',
+  em2: 'sans',
+  em3: 'sans',
+  caption: 'sans',
+  button: 'sans',
+};
+
 export type TypographyProps = React.HTMLAttributes<HTMLElement> & {
   variant: TypographyKey;
   element?: React.ElementType;
   bold?: boolean;
+  fontFamily?: FontFamily;
   textColor?: {
     on: keyof ThemeColorsForeground;
     variant: keyof ThemeColorVariants;
@@ -41,6 +65,7 @@ export const Typography: React.FC<TypographyProps> = props => {
   const {
     variant,
     element,
+    fontFamily,
     textColor = {
       on: 'onBackground',
       variant: 'standard',
@@ -51,14 +76,20 @@ export const Typography: React.FC<TypographyProps> = props => {
   } = props;
   const theme = useTheme();
   const Element = element ?? variantMapping[variant];
+  const fontFamilyValue =
+    fontFamily !== undefined
+      ? FONT_FAMILY[fontFamily]
+      : FONT_FAMILY[defaultFontFamilyMapping[variant]];
+  const color = theme.colors.theme[textColor.on][textColor.variant];
 
   return (
     <Element
       css={[
         theme.typography[variant],
         css`
-          color: ${theme.colors.theme[textColor.on][textColor.variant]};
+          color: ${color};
           font-weight: ${bold ? 700 : undefined};
+          font-family: ${fontFamilyValue};
         `,
       ]}
       {...restProps}
--- src/client/styles/typography.ts
+++ src/client/styles/typography.ts
@@ -3,86 +3,77 @@ const serifFontFamily =
 
 const sansSerifFontFamily = 'Helvetica, Verdana, Arial, sans-serif';
 
+export const FONT_FAMILY: Record<FontFamily, string> = {
+  serif: serifFontFamily,
+  sans: sansSerifFontFamily,
+};
+
+export type FontFamily = 'serif' | 'sans';
+
 export const TYPOGRAPHY = {
   h1: {
     fontSize: '4rem',
     fontWeight: 700,
-    fontFamily: serifFontFamily,
   },
   h2: {
     fontSize: '3rem',
     fontWeight: 700,
-    fontFamily: serifFontFamily,
   },
   h3: {
     fontSize: '2.5rem',
     fontWeight: 700,
-    fontFamily: serifFontFamily,
   },
   h4: {
     fontSize: '2rem',
     fontWeight: 700,
-    fontFamily: serifFontFamily,
   },
   h5: {
     fontSize: '1.5rem',
     fontWeight: 700,
-    fontFamily: serifFontFamily,
   },
   h6: {
     fontSize: '1.25rem',
     fontWeight: 700,
-    fontFamily: serifFontFamily,
   },
   h7: {
     fontSize: '1rem',
     fontWeight: 400,
-    fontFamily: serifFontFamily,
   },
   body1: {
     fontSize: '1.25rem',
     fontWeight: 400,
-    fontFamily: sansSerifFontFamily,
   },
   body2: {
     fontSize: '1rem',
     fontWeight: 400,
-    fontFamily: sansSerifFontFamily,
   },
   body3: {
     fontSize: '0.875rem',
     fontWeight: 400,
-    fontFamily: sansSerifFontFamily,
   },
   body4: {
     fontSize: '0.75rem',
     fontWeight: 400,
-    fontFamily: sansSerifFontFamily,
   },
   em1: {
     fontSize: '1.25rem',
     fontWeight: 700,
-    fontFamily: sansSerifFontFamily,
   },
   em2: {
     fontSize: '1rem',
     fontWeight: 700,
-    fontFamily: sansSerifFontFamily,
   },
   em3: {
     fontSize: '0.875rem',
     fontWeight: 700,
-    fontFamily: sansSerifFontFamily,
   },
   caption: {
     fontSize: '0.625rem',
     fontWeight: 400,
-    fontFamily: sansSerifFontFamily,
   },
   button: {
     fontSize: '1rem',
     fontWeight: 700,
-    fontFamily: sansSerifFontFamily,
   },
 };
 
