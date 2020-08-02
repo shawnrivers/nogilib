6a8da9f353e9d4b49290832d87d1b4bf9bccfc36
Shawn Rivers <kaushou@gmail.com>
2020-08-02 22:45:29 +0900

    Fix position counter styles

--- src/client/features/Member/components/PositionCounter/index.tsx
+++ src/client/features/Member/components/PositionCounter/index.tsx
@@ -35,121 +35,146 @@ export const PositionCounter: React.FC<PositionCounterProps> = props => {
         overflow: hidden;
       `}
     >
-      {selected !== 0 ? (
-        <div
-          css={css`
-            display: flex;
-            line-height: 28px;
-            width: ${(selected / (under + selected)) * 100}%;
+      <div
+        css={css`
+          display: flex;
+          line-height: 28px;
+          width: 100%;
 
-            &:not(:last-child) {
-              border-color: ${theme.colors.theme.onBackground.standard};
-              border-radius: 0;
-              border-right-style: solid;
-              border-right-width: 2px;
-            }
-          `}
-        >
-          {center !== 0 ? (
-            <div
-              css={css`
-                background-color: ${theme.colors.theme[
-                  POSITION_STYLES.center.backgroundColor
-                ][POSITION_STYLES.center.backgroundColorVariant]};
-                border-color: ${theme.colors.theme[
-                  POSITION_STYLES.center.foregroundColor
-                ][POSITION_STYLES.center.textColorVariant]};
-                text-align: center;
-                z-index: 4;
-                width: ${(center / selected) * 100}%;
-              `}
-              style={{ width: `${(center / selected) * 100}%` }}
-            >
-              <Typography
-                variant="body2"
-                element="span"
-                textColor={{ on: 'onPrimary', variant: 'standard' }}
-                bold
-              >
-                {center}
-              </Typography>
-            </div>
-          ) : null}
-          {fukujin - center !== 0 ? (
-            <div
-              css={css`
-                background-color: ${theme.colors.theme[
-                  POSITION_STYLES.fukujin.backgroundColor
-                ][POSITION_STYLES.fukujin.backgroundColorVariant]};
-                border-color: ${theme.colors.theme[
-                  POSITION_STYLES.fukujin.foregroundColor
-                ][POSITION_STYLES.fukujin.textColorVariant]};
-                text-align: center;
-                z-index: 3;
-                width: ${((fukujin - center) / selected) * 100}%;
-              `}
-            >
-              <Typography
-                variant="body2"
-                element="span"
-                textColor={{ on: 'onSecondary', variant: 'standard' }}
-                bold
-              >
-                {fukujin - center}
-              </Typography>
-            </div>
-          ) : null}
-          {selected - fukujin !== 0 ? (
-            <div
-              css={css`
-                background-color: ${theme.colors.theme[
-                  POSITION_STYLES.selected.backgroundColor
-                ][POSITION_STYLES.selected.backgroundColorVariant]};
-                border-color: ${theme.colors.theme[
-                  POSITION_STYLES.selected.foregroundColor
-                ][POSITION_STYLES.selected.textColorVariant]};
-                text-align: center;
-                z-index: 3;
-                width: ${((selected - fukujin) / selected) * 100}%;
-              `}
-            >
-              <Typography
-                variant="body2"
-                element="span"
-                textColor={{ on: 'onSecondary', variant: 'standard' }}
-                bold
-              >
-                {selected - fukujin}
-              </Typography>
-            </div>
-          ) : null}
-        </div>
-      ) : null}
-      {under !== 0 ? (
-        <div
-          css={css`
-            background-color: ${theme.colors.theme[
-              POSITION_STYLES.under.backgroundColor
-            ][POSITION_STYLES.under.backgroundColorVariant]};
-            border-color: ${theme.colors.theme[
-              POSITION_STYLES.under.foregroundColor
-            ][POSITION_STYLES.under.textColorVariant]};
-            text-align: center;
-            line-height: 28px;
-            z-index: 3;
-            width: ${(under / (under + selected)) * 100}%;
-          `}
-        >
-          <Typography
-            variant="body2"
-            element="span"
-            textColor={{ on: 'onSurface', variant: 'standard' }}
-            bold
+          &:not(:last-child) {
+            border-color: ${theme.colors.theme.onBackground.standard};
+            border-radius: 0;
+            border-right-style: solid;
+            border-right-width: 2px;
+          }
+        `}
+      >
+        {center !== 0 && (
+          <div
+            css={css`
+              background-color: ${theme.colors.theme[
+                POSITION_STYLES.center.backgroundColor
+              ][POSITION_STYLES.center.backgroundColorVariant]};
+              border-color: ${theme.colors.theme[
+                POSITION_STYLES.center.foregroundColor
+              ][POSITION_STYLES.center.textColorVariant]};
+              text-align: center;
+              z-index: 4;
+              width: ${(center / (under + selected)) * 100}%;
+              min-width: max-content;
+            `}
           >
-            {under}
-          </Typography>
-        </div>
-      ) : null}
+            <Typography
+              variant="body2"
+              element="span"
+              textColor={{
+                on: POSITION_STYLES.center.foregroundColor,
+                variant: POSITION_STYLES.center.textColorVariant,
+              }}
+              bold
+              css={css`
+                margin: 0 0.2em;
+              `}
+            >
+              {center}
+            </Typography>
+          </div>
+        )}
+        {fukujin - center !== 0 && (
+          <div
+            css={css`
+              background-color: ${theme.colors.theme[
+                POSITION_STYLES.fukujin.backgroundColor
+              ][POSITION_STYLES.fukujin.backgroundColorVariant]};
+              border-color: ${theme.colors.theme[
+                POSITION_STYLES.fukujin.foregroundColor
+              ][POSITION_STYLES.fukujin.textColorVariant]};
+              text-align: center;
+              z-index: 3;
+              width: ${((fukujin - center) / (under + selected)) * 100}%;
+              min-width: max-content;
+            `}
+          >
+            <Typography
+              variant="body2"
+              element="span"
+              textColor={{
+                on: POSITION_STYLES.fukujin.foregroundColor,
+                variant: POSITION_STYLES.fukujin.textColorVariant,
+              }}
+              bold
+              css={css`
+                margin: 0 0.2em;
+              `}
+            >
+              {fukujin - center}
+            </Typography>
+          </div>
+        )}
+        {selected - fukujin !== 0 && (
+          <div
+            css={css`
+              background-color: ${theme.colors.theme[
+                POSITION_STYLES.selected.backgroundColor
+              ][POSITION_STYLES.selected.backgroundColorVariant]};
+              border-color: ${theme.colors.theme[
+                POSITION_STYLES.selected.foregroundColor
+              ][POSITION_STYLES.selected.textColorVariant]};
+              text-align: center;
+              z-index: 3;
+              width: ${((selected - fukujin) / (under + selected)) * 100}%;
+              min-width: max-content;
+            `}
+          >
+            <Typography
+              variant="body2"
+              element="span"
+              textColor={{
+                on: POSITION_STYLES.selected.foregroundColor,
+                variant: POSITION_STYLES.selected.textColorVariant,
+              }}
+              bold
+              css={css`
+                margin: 0 0.2em;
+              `}
+            >
+              {selected - fukujin}
+            </Typography>
+          </div>
+        )}
+        {under !== 0 && (
+          <div
+            css={css`
+              background-color: ${theme.colors.theme[
+                POSITION_STYLES.under.backgroundColor
+              ][POSITION_STYLES.under.backgroundColorVariant]};
+              border-color: ${theme.colors.theme[
+                POSITION_STYLES.under.foregroundColor
+              ][POSITION_STYLES.under.textColorVariant]};
+              text-align: center;
+              line-height: 28px;
+              z-index: 4;
+              width: ${(under / (under + selected)) * 100}%;
+              min-width: max-content;
+            `}
+          >
+            <Typography
+              variant="body2"
+              element="span"
+              textColor={{
+                on: POSITION_STYLES.under.foregroundColor,
+                variant: POSITION_STYLES.under.textColorVariant,
+              }}
+              bold
+              css={css`
+                margin: 0 0.2em;
+              `}
+            >
+              {under}
+            </Typography>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
