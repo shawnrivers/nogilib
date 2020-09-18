f9d8ddb18d3b707876ca1024f6e2a54b57376425
Shawn Rivers <kaushou@gmail.com>
2020-09-18 10:49:13 +0900

    Add trailing slash to URLs

--- src/client/utils/urls.ts
+++ src/client/utils/urls.ts
@@ -2,18 +2,18 @@ export const FILTER_PARAM_KEY = 'filter';
 
 export type DiscographyUrlFilter = 'all' | 'singles' | 'albums';
 export const getDiscographyUrl = (filter?: DiscographyUrlFilter): string =>
-  filter ? `/discography?${FILTER_PARAM_KEY}=${filter}` : '/discography';
+  filter ? `/discography/?${FILTER_PARAM_KEY}=${filter}` : '/discography/';
 
 export const getAlbumUrl = (albumTitleKey: string): string =>
-  `/discography/${albumTitleKey}`;
+  `/discography/${albumTitleKey}/`;
 
 export const getSongUrl = (sonTitleKey: string): string =>
-  `/songs/${sonTitleKey}`;
+  `/songs/${sonTitleKey}/`;
 
 export type MembersUrlFilter = 'all' | 'current' | 'graduated';
 export const getMembersUrl = (filter?: MembersUrlFilter): string =>
-  filter ? `/members?${FILTER_PARAM_KEY}=${filter}` : '/members';
+  filter ? `/members/?${FILTER_PARAM_KEY}=${filter}` : '/members/';
 
-export const getMemberUrl = (memberName: string) => `/members/${memberName}`;
+export const getMemberUrl = (memberName: string) => `/members/${memberName}/`;
 
-export const getSearchUrl = () => '/search';
+export const getSearchUrl = () => '/search/';
