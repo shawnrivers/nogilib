7dc0cee0e8b026a88e067c5cc2e3b89435296c13
Shawn Rivers <kaushou@gmail.com>
2020-09-23 19:56:04 +0900

    Add toNth strings util function

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -8,6 +8,33 @@ export const classNames = (
 ): string | undefined =>
   classNames.reduce((acc, curr) => (curr ? acc + ' ' + curr : acc));
 
+export function isNumberCountable(num: string | number): boolean {
+  const number = Number(num);
+  return number > 0;
+}
+
+export function toNth(num: string | number): string | null {
+  if (isNumberCountable(num)) {
+    const number = Number(num);
+
+    const remainderByTen = number % 10;
+    const remainderByHundred = number % 100;
+    if (remainderByTen === 1 && remainderByHundred !== 11) {
+      return num + 'st.';
+    }
+    if (remainderByTen === 2 && remainderByHundred !== 12) {
+      return num + 'nd.';
+    }
+    if (remainderByTen === 3 && remainderByHundred !== 13) {
+      return num + 'rd.';
+    } else {
+      return num + 'th.';
+    }
+  } else {
+    return null;
+  }
+}
+
 export const toCdNumber = (num: string | number): string => {
   const number = Number(num);
 
