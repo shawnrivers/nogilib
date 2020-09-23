0bed0a932efba90d44a21fcdec042e127a48b6a8
Shawn Rivers <kaushou@gmail.com>
2020-09-23 22:36:44 +0900

    Remove unused methods

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,13 +1,3 @@
-import { Language } from 'client/types/language';
-
-export const getUrlWithTrailingSlash = (url: string): string =>
-  url.slice(-1) !== '/' ? url + '/' : url;
-
-export const classNames = (
-  ...classNames: (string | undefined)[]
-): string | undefined =>
-  classNames.reduce((acc, curr) => (curr ? acc + ' ' + curr : acc));
-
 export function isNumberCountable(num: string | number): boolean {
   const number = Number(num);
   return number > 0;
@@ -36,77 +26,15 @@ export function toNth(num: string | number): string | null {
 }
 
 export const toCdNumber = (num: string | number): string => {
-  const number = Number(num);
+  const nTh = toNth(num);
 
-  if (number) {
-    const remainderByTen = number % 10;
-    const remainderByHundred = number % 100;
-    if (remainderByTen === 1 && remainderByHundred !== 11) {
-      return num + 'st.';
-    }
-    if (remainderByTen === 2 && remainderByHundred !== 12) {
-      return num + 'nd.';
-    }
-    if (remainderByTen === 3 && remainderByHundred !== 13) {
-      return num + 'rd.';
-    } else {
-      return num + 'th.';
-    }
+  if (nTh) {
+    return nTh;
   } else {
     if (num === 'U') {
-      return 'Under';
+      return 'under';
     }
 
     return num.toString();
   }
 };
-
-export const toNumberWithLocale = (
-  num: string,
-  language: Language,
-  type?: 'cd' | 'row'
-): string => {
-  let counter = {
-    ja: '',
-    zh: '',
-  };
-
-  switch (type) {
-    case 'cd':
-      counter = {
-        ja: '枚',
-        zh: '张',
-      };
-      break;
-    case 'row':
-      counter = {
-        ja: '列',
-        zh: '排',
-      };
-      break;
-    default:
-      counter = {
-        ja: '枚',
-        zh: '张',
-      };
-      break;
-  }
-
-  if (language === 'en') {
-    return toCdNumber(num);
-  }
-
-  const number = Number(num);
-
-  if (number) {
-    if (language === 'ja') {
-      return num + counter + '目';
-    }
-    return '第' + num + counter;
-  }
-
-  return 'under';
-};
-
-export const commaTextArray = (array: string[]): string =>
-  array.reduce((acc, curr) => `${acc}, ${curr}`);
