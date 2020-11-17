621f2115d35ca51ba8a21f13e4ad26ca1e004791
Shawn Rivers <kaushou@gmail.com>
2020-11-17 22:49:05 +0900

    feat(member): add position badge to member card accessory

--- src/client/components/molecules/cards/MemberCard/index.tsx
+++ src/client/components/molecules/cards/MemberCard/index.tsx
@@ -7,6 +7,71 @@ import {
 } from 'client/components/atoms/Typography';
 import { Card, CardProps } from 'client/components/atoms/Card';
 import { GridMemberImage } from 'client/components/atoms/images/GridMemberImage';
+import { PositionType } from 'server/actors/Members/constants/position';
+import { useAppTheme } from 'client/styles/tokens';
+import { BORDER_RADIUS } from 'client/styles/borderRadius';
+import { POSITION_STYLES } from 'client/features/Member/constants/positionStyles';
+
+const PositionBadge: React.FC<{
+  position: PositionType.Center | PositionType.Fukujin;
+  borderRadius: MemberCardProps['borderRadius'];
+}> = props => {
+  const { position, borderRadius } = props;
+  const theme = useAppTheme();
+
+  const positionStylesKey =
+    position === PositionType.Center
+      ? POSITION_STYLES.center
+      : POSITION_STYLES.fukujin;
+
+  const badgeStyles = React.useMemo(() => {
+    const backgroundColor =
+      theme.colors.theme[positionStylesKey.backgroundColor][
+        positionStylesKey.backgroundColorVariant
+      ];
+
+    const borderRadiusStyle = `0
+    ${BORDER_RADIUS[borderRadius ?? 's']} 0
+    ${BORDER_RADIUS.s}`;
+
+    const borderColor =
+      theme.colors.theme[positionStylesKey.foregroundColor][
+        positionStylesKey.textColorVariant
+      ];
+
+    return css`
+      display: inline-block;
+      border-radius: ${borderRadiusStyle};
+      border-width: 2px;
+      border-style: solid;
+      border-color: ${borderColor};
+      background-color: ${backgroundColor};
+      width: 24px;
+      height: 28px;
+      text-align: center;
+      box-shadow: ${theme.elevation[1].boxShadow};
+    `;
+  }, [positionStylesKey, borderRadius, theme]);
+
+  return (
+    <div css={badgeStyles}>
+      <Typography
+        variant="body2"
+        element="span"
+        bold
+        textColor={{
+          on: positionStylesKey.foregroundColor,
+          variant: positionStylesKey.textColorVariant,
+        }}
+        css={css`
+          line-height: 28px;
+        `}
+      >
+        {position === PositionType.Center ? 'C' : 'F'}
+      </Typography>
+    </div>
+  );
+};
 
 export type MemberCardProps = CardProps & {
   profileImage: string;
@@ -14,6 +79,7 @@ export type MemberCardProps = CardProps & {
   lang: string;
   nameElement?: TypographyProps['element'];
   textSize?: TypographyProps['variant'];
+  position?: PositionType;
 };
 
 export const MemberCard: React.FC<MemberCardProps> = props => {
@@ -25,11 +91,22 @@ export const MemberCard: React.FC<MemberCardProps> = props => {
     textSize = 'em2',
     borderRadius = 's',
     padding = 's',
+    position,
     ...cardProps
   } = props;
 
   return (
-    <Card borderRadius={borderRadius} padding={padding} {...cardProps}>
+    <Card
+      borderRadius={borderRadius}
+      padding={padding}
+      accessory={
+        position === PositionType.Center ||
+        position === PositionType.Fukujin ? (
+          <PositionBadge position={position} borderRadius={borderRadius} />
+        ) : undefined
+      }
+      {...cardProps}
+    >
       <article>
         <GridMemberImage src={profileImage} alt="" />
         <Typography
