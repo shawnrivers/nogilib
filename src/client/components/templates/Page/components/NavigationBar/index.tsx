a3e43780ebbcd2738bcc8c975b6eb21e76543748
Shawn Rivers <kaushou@gmail.com>
2020-11-26 22:31:00 +0900

    fix: remove unnecessary property

--- src/client/components/templates/Page/components/NavigationBar/index.tsx
+++ src/client/components/templates/Page/components/NavigationBar/index.tsx
@@ -176,7 +176,6 @@ const Settings: React.FC = () => {
           open: { scale: 1, opacity: 1 },
           closed: { scale: 0, opacity: 0 },
         }}
-        exit={{ scale: 0, opacity: 0 }}
         transition={{ duration: 0.2 }}
         css={css`
           position: absolute;
