6a42ba5d210a697e771a17e89cf1385fd0e87347
Shawn Rivers <kaushou@gmail.com>
2020-12-20 13:04:41 +0900

    feat: use site metadata hook in Helmet

--- src/client/layouts/Helmet.tsx
+++ src/client/layouts/Helmet.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { Helmet as ReactHelmet } from 'react-helmet';
 import { useAppContext } from 'client/hooks/useAppContext';
+import { useSiteMetadata } from 'client/hooks/useSiteMetadata';
 
 export const Helmet: React.FC = () => {
   const { language } = useAppContext();
+  const { title, siteUrl, description } = useSiteMetadata();
 
   return (
     <ReactHelmet>
@@ -12,19 +14,13 @@ export const Helmet: React.FC = () => {
         name="viewport"
         content="width=device-width, initial-scale=1, viewport-fit=cover"
       />
-      <meta name="title" content="NOGILIB" />
-      <meta
-        name="description"
-        content="NOGILIB is a Gatsby powered web application aiming at showing the information about Nogizaka46 in a user-friendly way."
-      />
-      <meta name="url" content="https://shawnrivers.github.io/nogilib/" />
-      <meta name="og:title" content="NOGILIB" />
-      <meta
-        name="og:description"
-        content="NOGILIB is a Gatsby powered web application aiming at showing the information about Nogizaka46 in a user-friendly way."
-      />
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+      <meta name="url" content={siteUrl} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
       <meta name="og:type" content="website" />
-      <meta name="og:url" content="https://shawnrivers.github.io/nogilib/" />
+      <meta name="og:url" content={siteUrl} />
       <meta
         property="og:image"
         content="https://raw.githubusercontent.com/shawnrivers/nogilib/master/src/assets/images/design/preview.jpg"
@@ -32,13 +28,13 @@ export const Helmet: React.FC = () => {
       <meta name="og:image:type" content="image/jpeg" />
       <meta name="og:image:width" content="2560" />
       <meta name="og:image:height" content="1280" />
-      <meta name="og:image:alt" content="NOGILIB" />
+      <meta name="og:image:alt" content={title} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content="@yuxiao_he" />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <title>NOGILIB</title>
+      <title>{title}</title>
       <html lang={language} />
     </ReactHelmet>
   );
