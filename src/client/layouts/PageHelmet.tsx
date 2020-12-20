07c42ab1168da6203e5af4d8bdd5a0309f8484be
Shawn Rivers <kaushou@gmail.com>
2020-12-20 13:42:20 +0900

    feat: add text transform feature in PageHelmet

--- src/client/layouts/PageHelmet.tsx
+++ src/client/layouts/PageHelmet.tsx
@@ -1,17 +1,40 @@
 import * as React from 'react';
 import { Helmet as ReactHelmet } from 'react-helmet';
 import { useSiteMetadata } from 'client/hooks/useSiteMetadata';
+import { capitalizeFirstLetter, capitalizeText } from 'utils/strings';
 
 type PageHelmetProps = {
   title?: string;
   description?: string;
+  options?: {
+    textTransform?: 'capitalize-first-letter' | 'capitalize' | 'none';
+  };
 };
 
 export const PageHelmet: React.FC<PageHelmetProps> = props => {
-  const { title, description } = props;
+  const {
+    title,
+    description,
+    options = { textTransform: 'capitalize-first-letter' },
+  } = props;
   const { title: siteTitle, description: siteDescription } = useSiteMetadata();
 
-  const pageTitle = title ? `${title} | ${siteTitle}` : siteTitle;
+  const pageTitleText = title ? `${title} | ${siteTitle}` : siteTitle;
+
+  let pageTitle;
+
+  switch (options.textTransform) {
+    case 'capitalize':
+      pageTitle = capitalizeText(pageTitleText);
+      break;
+    case 'capitalize-first-letter':
+      pageTitle = capitalizeFirstLetter(pageTitleText);
+      break;
+    default:
+      pageTitle = pageTitleText;
+      break;
+  }
+
   const pageDescription = description ?? siteDescription;
 
   return (
