39c477f54092364933e0647309725cada9ea80f7
Shawn Rivers <kaushou@gmail.com>
2020-12-23 13:42:31 +0900

    refactor: rename old GatsbyImage component as DynamicImage

--- src/client/components/atoms/images/DynamicImage/index.tsx
+++ src/client/components/atoms/images/DynamicImage/index.tsx
@@ -2,11 +2,14 @@ import Img from 'gatsby-image';
 import React from 'react';
 import { useAllImagesContext } from 'client/store/images/useAllImagesContext';
 
-export type GatsbyImageProps = {
+export type DynamicImageProps = {
   src: string;
 } & Omit<React.ComponentProps<typeof Img>, 'fluid'>;
 
-export const GatsbyImage: React.FC<GatsbyImageProps> = ({ src, ...props }) => {
+export const DynamicImage: React.FC<DynamicImageProps> = ({
+  src,
+  ...props
+}) => {
   const { imgStyle, ...restProps } = props;
   const { allImages } = useAllImagesContext();
 
