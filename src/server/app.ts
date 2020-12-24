fd7d501154692b557c2eba9c39c09daeba6fd226
Shawn Rivers <kaushou@gmail.com>
2020-12-24 19:22:40 +0900

    refactor(server): generate page data for discography

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,4 +1,3 @@
-import * as fs from 'fs';
 import { Members } from 'server/actors/Members';
 import { membersRawArray } from 'server/actors/Members/raw';
 import { Songs } from 'server/actors/Songs';
@@ -7,7 +6,10 @@ import { Units } from 'server/actors/Units';
 import { unitsRawArray } from 'server/actors/Units/raw';
 import { discographyRawArray } from 'server/actors/Discography/raw/editor';
 import { Discography } from 'server/actors/Discography';
+import { writeJSONFile } from 'server/utils/files';
+import { getDiscographyPageData } from 'server/pages/discography';
 
+// Generate raw data
 const songs = new Songs(songsRawArray);
 const members = new Members(membersRawArray);
 const units = new Units(unitsRawArray);
@@ -17,6 +19,7 @@ const singlesRawArray = discography.getSinglesRawArray();
 const albumsRawArray = discography.getAlbumsRawArray();
 const otherCdsRawArray = discography.getOtherCdsRawArray();
 
+// Convert to result data
 const songsResultArray = songs.convertSongs({
   singlesRawArray,
   singlesRawObject: discography.getSinglesRawObject(),
@@ -35,21 +38,15 @@ const membersResultArray = members.convertMembers({
 const unitsResultArray = units.convertUnits({
   songsRawArray: songs.rawArray,
 });
-const discographyResultArray = discography.convertDiscography({
-  songsRawObject: songs.rawObject,
-});
+// const discographyResultArray = discography.convertDiscography({
+//   songsRawObject: songs.rawObject,
+// });
 
-const writeFile = (path: string, data: any[]) => {
-  fs.writeFile(path, JSON.stringify(data, null, 2), err => {
-    if (err) {
-      console.warn(err);
-    } else {
-      console.log(`JSON saved in: ${path}`);
-    }
-  });
-};
+// Convert to page data
+const dicographyPageData = getDiscographyPageData(discography);
 
-writeFile('./src/data/members.json', membersResultArray);
-writeFile('./src/data/songs.json', songsResultArray);
-writeFile('./src/data/units.json', unitsResultArray);
-writeFile('./src/data/discography.json', discographyResultArray);
+// Store in JSON files
+writeJSONFile('./src/data/members.json', membersResultArray);
+writeJSONFile('./src/data/songs.json', songsResultArray);
+writeJSONFile('./src/data/units.json', unitsResultArray);
+writeJSONFile('./src/data/discography.json', dicographyPageData);
