58af8f67350a9941335fd3f3925cbc113d3ae3ac
Shawn Rivers <kaushou@gmail.com>
2020-12-25 14:41:20 +0900

    refactor(client): use the new album data and restructure some file structure

---
+++ src/pages/discography/{AlbumJson.key}.tsx
@@ -0,0 +1,142 @@
+import { graphql } from 'gatsby';
+import { FluidObject } from 'gatsby-image';
+import * as React from 'react';
+import { AlbumPage } from 'client/features/Album';
+import { AlbumPageData } from 'server/pages/album';
+import { arrayToObject } from 'utils/arrays';
+
+export const query = graphql`
+  query($key: String!) {
+    albumJson(key: { eq: $key }) {
+      title
+      type
+      number
+      artworks {
+        url {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        type
+      }
+      release
+      songs {
+        key
+        title
+        type
+        inCdType
+      }
+      centers {
+        name
+        nameNotations {
+          firstName
+          firstNameEn
+          lastName
+          lastNameEn
+        }
+        albumProfileImage {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+type AlbumPageDataNode = {
+  title: AlbumPageData[0]['title'];
+  type: AlbumPageData[0]['type'];
+  number: AlbumPageData[0]['number'];
+  artworks: {
+    url: {
+      childImageSharp: {
+        fluid: FluidObject;
+      };
+    };
+    type: AlbumPageData[0]['artworks'][0]['type'];
+  }[];
+  release: AlbumPageData[0]['release'];
+  songs: AlbumPageData[0]['songs'];
+  centers: {
+    name: AlbumPageData[0]['centers'][0]['name'];
+    nameNotations: Pick<
+      AlbumPageData[0]['centers'][0]['nameNotations'],
+      'firstName' | 'firstNameEn' | 'lastName' | 'lastNameEn'
+    >;
+    albumProfileImage: {
+      childImageSharp: {
+        fluid: FluidObject;
+      };
+    };
+  }[];
+};
+
+export type AlbumPageProps = {
+  title: AlbumPageDataNode['title'];
+  type: AlbumPageDataNode['type'];
+  number: AlbumPageDataNode['number'];
+  artworks: {
+    fluid: AlbumPageDataNode['artworks'][0]['url']['childImageSharp']['fluid'];
+    type: AlbumPageDataNode['artworks'][0]['type'];
+  }[];
+  release: AlbumPageDataNode['release'];
+  tracks: {
+    key: AlbumPageDataNode['songs'][0]['key'];
+    title: AlbumPageDataNode['songs'][0]['title'];
+    type: AlbumPageDataNode['songs'][0]['type'];
+    artworkFluid: AlbumPageDataNode['artworks'][0]['url']['childImageSharp']['fluid'];
+  }[];
+  centers: {
+    name: AlbumPageDataNode['centers'][0]['name'];
+    nameNotations: AlbumPageDataNode['centers'][0]['nameNotations'];
+    albumProfileImageFluid: AlbumPageDataNode['centers'][0]['albumProfileImage']['childImageSharp']['fluid'];
+  }[];
+};
+
+const AlbumPageContainer: React.FC<{
+  data: {
+    albumJson: AlbumPageDataNode;
+  };
+}> = props => {
+  const albumData = props.data.albumJson;
+
+  const artworks: AlbumPageProps['artworks'] = albumData.artworks.map(
+    artwork => ({
+      fluid: artwork.url.childImageSharp.fluid,
+      type: artwork.type,
+    })
+  );
+
+  const artworksObject = arrayToObject(artworks, 'type');
+  const tracks: AlbumPageProps['tracks'] = albumData.songs.map(song => ({
+    key: song.key,
+    title: song.title,
+    type: song.type,
+    artworkFluid: artworksObject[song.inCdType[0]].fluid,
+  }));
+
+  const centers: AlbumPageProps['centers'] = albumData.centers.map(center => ({
+    name: center.name,
+    nameNotations: center.nameNotations,
+    albumProfileImageFluid: center.albumProfileImage.childImageSharp.fluid,
+  }));
+
+  return (
+    <AlbumPage
+      title={albumData.title}
+      type={albumData.type}
+      number={albumData.number}
+      artworks={artworks}
+      release={albumData.release}
+      tracks={tracks}
+      centers={centers}
+    />
+  );
+};
+
+export default AlbumPageContainer;
