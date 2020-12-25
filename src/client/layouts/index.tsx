1faee20aeed9a4d39847dedf774b2e870e922abe
Shawn Rivers <kaushou@gmail.com>
2020-12-25 16:23:58 +0900

    perf: remove all images query

--- src/client/layouts/index.tsx
+++ src/client/layouts/index.tsx
@@ -4,21 +4,18 @@ import { Page } from 'client/components/templates/Page';
 import { Helmet } from 'client/layouts/Helmet';
 import { LanguageContextProvider } from 'client/store/language/context';
 import { EmotionThemeProvider } from 'client/store/emotion/provider';
-import { AllImagesContextProvider } from 'client/store/images/context';
 import { ThemeContextProvider } from 'client/store/theme/context';
 
 const AppLayout: React.FC<RouteComponentProps> = props => {
   return (
-    <AllImagesContextProvider>
-      <LanguageContextProvider>
-        <ThemeContextProvider>
-          <Helmet />
-          <EmotionThemeProvider>
-            <Page>{props.children}</Page>
-          </EmotionThemeProvider>
-        </ThemeContextProvider>
-      </LanguageContextProvider>
-    </AllImagesContextProvider>
+    <LanguageContextProvider>
+      <ThemeContextProvider>
+        <Helmet />
+        <EmotionThemeProvider>
+          <Page>{props.children}</Page>
+        </EmotionThemeProvider>
+      </ThemeContextProvider>
+    </LanguageContextProvider>
   );
 };
 
--- src/client/store/images/hook.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { AllImagesContext } from 'client/store/images/context';
-
-export const useAllImagesContext = () => {
-  return React.useContext(AllImagesContext);
-};
