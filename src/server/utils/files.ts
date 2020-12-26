cd8fee3c31b1dad6479cf575311b79c2472eb283
Shawn Rivers <kaushou@gmail.com>
2020-12-26 14:57:17 +0900

    refactor(server): generate page data for search

--- src/server/utils/files.ts
+++ src/server/utils/files.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 
-export function writeJSONFile(path: string, data: any[]) {
+export function writeJSONFile(path: string, data: any) {
   fs.writeFile(path, JSON.stringify(data, null, 2), err => {
     if (err) {
       throw err;
