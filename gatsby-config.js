58291473b03200b7e1947cf3f6978e0451b86220
Shawn Rivers <kaushou@gmail.com>
2020-12-26 16:55:04 +0900

    build: replace gatsby-plugin-lunr with FlexSearch for text searching

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,65 +71,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-lunr',
-      options: {
-        languages: [{ name: 'en' }, { name: 'ja' }],
-        fields: [
-          { name: 'name', store: true, attributes: { boost: 20 } },
-          { name: 'title', store: true },
-          { name: 'number', store: true },
-          { name: 'key', store: true },
-          { name: 'nameKey', store: true },
-          { name: 'artwork', store: true },
-          { name: 'profileImage', store: true },
-          { name: 'nameNotations', store: true },
-          { name: 'songType', store: true },
-          { name: 'single', store: true },
-          { name: 'album', store: true },
-          { name: 'type', store: true },
-          { name: 'cdType', store: true },
-          { name: 'join', store: true },
-        ],
-        resolvers: {
-          MembersJson: {
-            name: node =>
-              node.nameNotations.lastName +
-              node.nameNotations.firstName +
-              ' ' +
-              node.nameNotations.lastNameFurigana +
-              node.nameNotations.firstNameFurigana +
-              ' ' +
-              node.name,
-            key: node => node.name,
-            nameNotations: node => node.nameNotations,
-            profileImage: node =>
-              node.profileImages.gallery[node.profileImages.gallery.length - 1],
-            join: node => node.join,
-            type: () => 'members',
-          },
-          DiscographyJson: {
-            name: node => node.title + ' ' + node.key,
-            title: node => node.title,
-            key: node => node.key,
-            number: node => node.number,
-            artwork: node => node.artworks[0],
-            cdType: node => node.type,
-            type: () => 'cds',
-          },
-          SongsJson: {
-            name: node => node.title + ' ' + node.key,
-            title: node => node.title,
-            key: node => node.key,
-            artwork: node => node.artwork,
-            songType: node => node.type,
-            single: node => node.single,
-            album: node => node.albums[0],
-            type: () => 'songs',
-          },
-        },
-      },
-    },
-    {
       resolve: 'gatsby-plugin-webpack-bundle-analyser-v2',
       options: {
         disable: !process.env.ANALYZE_BUNDLE_SIZE,
