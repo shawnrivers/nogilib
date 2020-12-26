b80a5837b902442cf894b48523b432057c6a3c38
Shawn Rivers <kaushou@gmail.com>
2020-12-26 19:21:10 +0900

    fix: fix failing tests

--- src/server/actors/Members/__tests__/converters.ts
+++ src/server/actors/Members/__tests__/converters.ts
@@ -71,26 +71,26 @@ describe('convertProfileImages', () => {
 
   test('should sort gallery by date', () => {
     expect(nanaseProfileImages.gallery).toEqual([
-      'members/singles/1/nishinonanase.jpg',
-      'members/singles/2/nishinonanase.jpg',
-      'members/singles/3/nishinonanase.jpg',
-      'members/singles/4/nishinonanase.jpg',
-      'members/singles/5/nishinonanase.jpg',
-      'members/singles/6/nishinonanase.jpg',
-      'members/singles/7/nishinonanase.jpg',
-      'members/singles/8/nishinonanase.jpg',
-      'members/singles/9/nishinonanase.jpg',
-      'members/singles/10/nishinonanase.jpg',
-      'members/singles/11/nishinonanase.jpg',
-      'members/singles/12/nishinonanase.jpg',
-      'members/singles/13/nishinonanase.jpg',
-      'members/singles/14/nishinonanase.jpg',
-      'members/singles/15/nishinonanase.jpg',
-      'members/singles/17/nishinonanase.jpg',
-      'members/singles/20/nishinonanase.jpg',
-      'members/singles/22/nishinonanase.jpg',
-      'members/others/nishinonanase_2019-09-21.jpg',
-      'members/others/nishinonanase_2020-06-11.jpg',
+      '../assets/images/members/others/nishinonanase_2020-06-11.jpg',
+      '../assets/images/members/others/nishinonanase_2019-09-21.jpg',
+      '../assets/images/members/singles/22/nishinonanase.jpg',
+      '../assets/images/members/singles/20/nishinonanase.jpg',
+      '../assets/images/members/singles/17/nishinonanase.jpg',
+      '../assets/images/members/singles/15/nishinonanase.jpg',
+      '../assets/images/members/singles/14/nishinonanase.jpg',
+      '../assets/images/members/singles/13/nishinonanase.jpg',
+      '../assets/images/members/singles/12/nishinonanase.jpg',
+      '../assets/images/members/singles/11/nishinonanase.jpg',
+      '../assets/images/members/singles/10/nishinonanase.jpg',
+      '../assets/images/members/singles/9/nishinonanase.jpg',
+      '../assets/images/members/singles/8/nishinonanase.jpg',
+      '../assets/images/members/singles/7/nishinonanase.jpg',
+      '../assets/images/members/singles/6/nishinonanase.jpg',
+      '../assets/images/members/singles/5/nishinonanase.jpg',
+      '../assets/images/members/singles/4/nishinonanase.jpg',
+      '../assets/images/members/singles/3/nishinonanase.jpg',
+      '../assets/images/members/singles/2/nishinonanase.jpg',
+      '../assets/images/members/singles/1/nishinonanase.jpg',
     ]);
   });
 
@@ -111,28 +111,28 @@ describe('convertProfileImages', () => {
 
   test('should use the corresponding file for single', () => {
     expect(nanaseSingleProfileImages[15].url).toEqual(
-      'members/singles/15/nishinonanase.jpg'
+      '../assets/images/members/singles/15/nishinonanase.jpg'
     );
   });
 
   test('should use the closest image if no corresponding file exists', () => {
     expect(nanaseSingleProfileImages[16].url).toEqual(
-      'members/singles/15/nishinonanase.jpg'
+      '../assets/images/members/singles/15/nishinonanase.jpg'
     );
     expect(nanaseSingleProfileImages[21].url).toEqual(
-      'members/singles/20/nishinonanase.jpg'
+      '../assets/images/members/singles/20/nishinonanase.jpg'
     );
     expect(nanaseSingleProfileImages[23].url).toEqual(
-      'members/singles/22/nishinonanase.jpg'
+      '../assets/images/members/singles/22/nishinonanase.jpg'
     );
     expect(nanaseSingleProfileImages[25].url).toEqual(
-      'members/others/nishinonanase_2019-09-21.jpg'
+      '../assets/images/members/others/nishinonanase_2019-09-21.jpg'
     );
     expect(nanaseAlbumProfileImages['U'].url).toEqual(
-      'members/singles/17/nishinonanase.jpg'
+      '../assets/images/members/singles/17/nishinonanase.jpg'
     );
     expect(nanaseDigitalProfileImages[2].url).toEqual(
-      'members/others/nishinonanase_2020-06-11.jpg'
+      '../assets/images/members/others/nishinonanase_2020-06-11.jpg'
     );
   });
 });
