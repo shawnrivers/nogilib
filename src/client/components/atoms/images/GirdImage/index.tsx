ef29106ef1ed8aae4184f4f5418770cb7460d9e7
Shawn Rivers <kaushou@gmail.com>
2020-12-27 15:44:43 +0900

    refactor: extend GridImage to make fixed as an available prop

--- src/client/components/atoms/images/GirdImage/index.tsx
+++ src/client/components/atoms/images/GirdImage/index.tsx
@@ -1,70 +1,12 @@
 /**@jsx jsx */
 import { css, jsx } from '@emotion/core';
 import * as React from 'react';
-import { FluidObject } from 'gatsby-image';
 import { componentElevationKey } from 'client/styles/tokens/elevation';
 import { commonStyles, useAppTheme } from 'client/styles/tokens';
 import { BorderRadiusKey } from 'client/styles/tokens/borderRadius';
-import {
-  StaticImage,
-  StaticImageProps,
-} from 'client/components/atoms/images/StaticImage';
+import { GatsbyImage } from 'client/components/atoms/images/GatsbyImage';
 
-type ImageProps = {
-  fluid: FluidObject;
-  borderRadius: BorderRadiusKey;
-  shadow: boolean;
-} & StaticImageProps;
-
-const Image: React.FC<ImageProps> = props => {
-  const { fluid, borderRadius, shadow, imgStyle, ...staticImageProps } = props;
-  const theme = useAppTheme();
-
-  return (
-    <div
-      css={css`
-        width: 100%;
-        height: 100%;
-        position: relative;
-        border-radius: ${commonStyles.borderRadius[borderRadius]};
-        box-shadow: ${shadow
-          ? theme.elevation[componentElevationKey.componentOnBackground]
-              .boxShadow
-          : 'none'};
-        overflow: hidden;
-      `}
-    >
-      <div
-        css={css`
-          background-color: ${theme.colors.theme.surface.variant0};
-          top: 0;
-          right: 0;
-          bottom: 0;
-          left: 0;
-          position: absolute;
-        `}
-      >
-        <StaticImage
-          fluid={fluid}
-          css={css`
-            display: block;
-            width: 100%;
-            height: 100%;
-            transform-origin: center;
-          `}
-          imgStyle={{
-            ...imgStyle,
-            objectFit: 'cover',
-            objectPosition: 'center top',
-          }}
-          {...staticImageProps}
-        />
-      </div>
-    </div>
-  );
-};
-
-export type GridImageProps = Omit<ImageProps, 'borderRadius' | 'shadow'> & {
+export type GridImageProps = React.ComponentProps<typeof GatsbyImage> & {
   borderRadius?: BorderRadiusKey;
   shadow?: boolean;
   ratio?: number;
@@ -74,17 +16,18 @@ export type GridImageProps = Omit<ImageProps, 'borderRadius' | 'shadow'> & {
 
 export const GridImage: React.FC<GridImageProps> = props => {
   const {
-    shadow = false,
     borderRadius = 's',
+    shadow = false,
     ratio = 1,
     fixedSize = false,
     className,
-    fluid,
+    imgStyle,
+    ...gatsbyImageProps
   } = props;
 
   const theme = useAppTheme();
 
-  const containerStyles = React.useMemo(() => {
+  const wrapperStyles = React.useMemo(() => {
     const baseStyles = css`
       position: relative;
       overflow: auto;
@@ -105,7 +48,7 @@ export const GridImage: React.FC<GridImageProps> = props => {
   }, [borderRadius, fixedSize, ratio, shadow, theme.elevation]);
 
   return (
-    <div css={containerStyles} className={className}>
+    <div css={wrapperStyles} className={className}>
       <div
         css={css`
           position: absolute;
@@ -115,7 +58,45 @@ export const GridImage: React.FC<GridImageProps> = props => {
           left: 0;
         `}
       >
-        <Image fluid={fluid} shadow={shadow} borderRadius={borderRadius} />
+        <div
+          css={css`
+            width: 100%;
+            height: 100%;
+            position: relative;
+            border-radius: ${commonStyles.borderRadius[borderRadius]};
+            box-shadow: ${shadow
+              ? theme.elevation[componentElevationKey.componentOnBackground]
+                  .boxShadow
+              : 'none'};
+            overflow: hidden;
+          `}
+        >
+          <div
+            css={css`
+              background-color: ${theme.colors.theme.surface.variant0};
+              top: 0;
+              right: 0;
+              bottom: 0;
+              left: 0;
+              position: absolute;
+            `}
+          >
+            <GatsbyImage
+              css={css`
+                display: block;
+                width: 100%;
+                height: 100%;
+                transform-origin: center;
+              `}
+              imgStyle={{
+                ...imgStyle,
+                objectFit: 'cover',
+                objectPosition: 'center top',
+              }}
+              {...gatsbyImageProps}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
