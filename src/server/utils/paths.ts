94799502ce01c70ea263ea863782f80f53b4176a
Shawn Rivers <kaushou@gmail.com>
2021-02-15 01:51:41 +0900

    refactor: fix data image path

--- src/server/utils/paths.ts
+++ src/server/utils/paths.ts
@@ -1,11 +1,11 @@
 /**
- * Convert the image file path to a relative path for Gatsby source filesystem
+ * Convert the image file path to a relative path for Next asset filesystem
  * @param imagePath Image path like `"photo-albums/akimotomanatsu_1.jpg"`
  */
 export function convertImageFilePath(imagePath: string): string {
-  if (imagePath.startsWith('../assets/images/')) {
+  if (imagePath.startsWith('/images/')) {
     return imagePath;
   }
 
-  return `../assets/images/${imagePath}`;
+  return `/images/${imagePath}`;
 }
