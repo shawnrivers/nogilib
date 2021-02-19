9a6eada8536973813c8b7f0a445cc7ac114037b2
Shawn Rivers <kaushou@gmail.com>
2021-02-19 20:03:55 +0900

    refactor: fix 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,15 @@
-import * as React from 'react';
-import { Typography } from 'client/components/atoms/Typography';
+import { useRouter } from 'next/router';
 import { PageContent } from 'client/components/templates/Page';
 import { useTranslations } from 'client/i18n/hooks/useTranslations';
-import { useLanguageContext } from 'client/store/language/hooks/useLanguageContext';
 
 const NotFoundPage: React.FC = () => {
   const { getTranslation } = useTranslations();
-  const { language } = useLanguageContext();
+  const { locale } = useRouter();
 
   return (
     <PageContent
-      title={{ text: getTranslation('page not found'), lang: language }}
-    >
-      <Typography variant="body1">Please enter the right URL</Typography>
-    </PageContent>
+      title={{ text: getTranslation('page not found'), lang: locale }}
+    ></PageContent>
   );
 };
 
