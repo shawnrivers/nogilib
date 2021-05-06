b175a7c6165188682b26e5cc688384228ee9dfcd
Shawn Rivers <kaushou@gmail.com>
2021-05-06 15:04:09 +0900

    test: setup axe for dev env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import 'focus-visible';
+import * as React from 'react';
 import type { AppProps } from 'next/app';
 import { EmotionThemeProvider } from 'client/store/emotion/provider';
 import { ThemeContextProvider } from 'client/store/theme/context';
 import { Page } from 'client/components/layout/Page';
 import { SearchContextProvider } from 'client/store/search/context';
+import { setupAxe } from 'client/libs/axe';
 
 const App: React.FC<AppProps> = props => {
   const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    setupAxe();
+  }, []);
 
   return (
     <ThemeContextProvider>
