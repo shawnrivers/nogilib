02707ea313ec0da5015f2b1d4c6f9946b4bb339c
Shawn Rivers <kaushou@gmail.com>
2021-05-14 02:11:05 +0900

    fix: exclude lazy image polyfill from SSR

--- src/client/components/atoms/image/Image/PolyfilledLazyImage/index.tsx
+++ src/client/components/atoms/image/Image/PolyfilledLazyImage/index.tsx
@@ -1,3 +1,5 @@
 import dynamic from 'next/dynamic';
 
-export const PolyfilledLazyImage = dynamic(() => import('./Component'));
+export const PolyfilledLazyImage = dynamic(() => import('./Component'), {
+  ssr: false,
+});
