a3ade04b627b590f04ffe3b7af6dc4276ea2970f
Shawn Rivers <kaushou@gmail.com>
2021-05-14 01:53:07 +0900

    refactor: separate image component that polyfills lazy-loading and keeps the aspect ratio

---
+++ src/client/components/atoms/image/AspectRatioImage/index.tsx
@@ -0,0 +1,36 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/core';
+import { Image, ImageProps } from '../Image';
+
+type AspectRatioImageProps = ImageProps & {
+  aspectRatio?: number;
+};
+
+export const AspectRatioImage: React.FC<AspectRatioImageProps> = props => {
+  const { aspectRatio = 1, className, ...restImgProps } = props;
+
+  return (
+    <div
+      css={css`
+        position: relative;
+        width: 100%;
+        padding-top: ${aspectRatio * 100}%;
+        overflow: hidden;
+      `}
+      className={className}
+    >
+      <Image
+        css={css`
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          object-position: top;
+        `}
+        {...restImgProps}
+      />
+    </div>
+  );
+};
