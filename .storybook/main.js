22915606aa449313fb4c4525dc6ae26b31cf9e9d
Shawn Rivers <kaushou@gmail.com>
2021-08-08 21:44:16 +0900

    fix: align storybook babel configs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
+    '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-knobs',
-    '@storybook/preset-typescript',
+    '@react-theming/storybook-addon',
   ],
   webpackFinal: async config => {
     config.resolve.mainFields = ['browser', 'module', 'main'];
@@ -13,27 +13,20 @@ module.exports = {
     config.resolve.alias['server'] = path.resolve(__dirname, '../src/server/');
     config.resolve.alias['utils'] = path.resolve(__dirname, '../src/utils/');
     config.resolve.alias['storybook'] = path.resolve(__dirname, 'libs/');
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      include: [
-        path.resolve(__dirname, '../src/client'),
-        path.resolve(__dirname, '../src/server'),
-        path.resolve(__dirname, '../src/utils'),
-        path.resolve(__dirname, '../.storybook'),
-      ],
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: [['react-app', { flow: false, typescript: true }]],
-            plugins: [
-              require.resolve('@babel/plugin-proposal-class-properties'),
-            ],
-          },
-        },
-      ],
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
     return config;
+  },
+  babel: async options => {
+    const presets = options.presets.slice();
+
+    // Modify the presets config of @babel/preset-react.
+    presets[2][1] = {
+      runtime: 'automatic',
+      importSource: '@emotion/react',
+    };
+
+    return {
+      ...options,
+      presets,
+    };
   },
 };
