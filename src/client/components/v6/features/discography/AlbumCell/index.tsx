8a79dc45c4c4a843292414503f4c8b4e2cb1230a
Shawn Rivers <kaushou@gmail.com>
2021-08-11 23:30:06 +0900

    style: refine album cell hover animation

--- src/client/components/v6/features/discography/AlbumCell/index.tsx
+++ src/client/components/v6/features/discography/AlbumCell/index.tsx
@@ -11,6 +11,30 @@ import { ImageUrl } from 'server/types/commons';
 import { DiscographyType } from 'server/actors/Discography/types';
 import { useIntl } from 'client/i18n/hooks/useIntl';
 import { getImgSrcSet } from 'client/utils/imgSrcSet';
+import { ArrowRightIcon } from 'client/components/v6/shared/icons/ArrowRightIcon';
+
+const imageInitialStyles = css`
+  transform: scale(1);
+`;
+const imageHoveredStyles = css`
+  transform: scale(1.2);
+`;
+
+const imageWrapperBeforeInitialStyles = css`
+  opacity: 0;
+`;
+const imageWrapperBeforeHoveredStyles = css`
+  opacity: 0.5;
+`;
+
+const arrowInitialStyles = css`
+  transform: translateX(-102%) scale(0);
+  opacity: 0;
+`;
+const arrowHoveredStyles = css`
+  transform: translateX(0) scale(1);
+  opacity: 1;
+`;
 
 export type AlbumCellProps = {
   href: string;
@@ -33,12 +57,51 @@ export const AlbumCell: React.FC<AlbumCellProps> = props => {
           vertical-align: bottom;
           cursor: pointer;
 
-          & img {
+          & .image {
             transition: transform 0.3s cubic-bezier(0, 0.5, 0.7, 1);
           }
 
-          &:hover img {
-            transform: scale(1.2);
+          & .image-wrapper::before {
+            transition: opacity 0.3s cubic-bezier(0, 0.5, 0.7, 1);
+          }
+
+          & .arrow {
+            transition: transform 0.3s cubic-bezier(0, 0.5, 0.7, 1),
+              opacity 0.3s cubic-bezier(0, 0.5, 0.7, 1);
+          }
+
+          &.focus-visible {
+            outline: 2px solid transparent;
+            outline-offset: 2px;
+          }
+
+          &:active .image,
+          &.focus-visible .image {
+            ${imageHoveredStyles};
+          }
+
+          &:active .image-wrapper::before,
+          &.focus-visible .image-wrapper::before {
+            ${imageWrapperBeforeHoveredStyles};
+          }
+
+          &:active .arrow,
+          &.focus-visible .arrow {
+            ${arrowHoveredStyles};
+          }
+
+          @media (hover: hover) and (pointer: fine) {
+            &:hover .image {
+              ${imageHoveredStyles};
+            }
+
+            &:hover .image-wrapper::before {
+              ${imageWrapperBeforeHoveredStyles};
+            }
+
+            &:hover .arrow {
+              ${arrowHoveredStyles};
+            }
           }
         `}
       >
@@ -53,40 +116,102 @@ export const AlbumCell: React.FC<AlbumCellProps> = props => {
         >
           <div
             css={css`
-              position: absolute;
-              z-index: 1;
-              bottom: ${commonStyles.spacing.xs};
-              right: ${commonStyles.spacing.xs};
-              border: 1px solid var(${getGlobalColorVarName('gray8')});
-              padding-top: ${commonStyles.spacing.xs};
-              padding-bottom: ${commonStyles.spacing.xs};
-              padding-left: ${commonStyles.spacing.xs};
-              padding-right: calc(${commonStyles.spacing.xs} + 2px);
-              background-color: var(${getGlobalColorVarName('white')});
-              line-height: 1;
-            `}
-          >
-            <Typography
-              variant="caption"
-              element="span"
-              css={css`
-                text-transform: uppercase;
-                font-style: italic;
-                color: var(${getGlobalColorVarName('gray8')});
-              `}
-            >
-              {caption}
-            </Typography>
-          </div>
-          <div
-            css={css`
               width: 100%;
               height: 100%;
               display: flex;
+              flex-direction: row-reverse;
               overflow: hidden;
               z-index: 0;
             `}
           >
+            <div
+              className="image-wrapper"
+              css={css`
+                flex: 1;
+                overflow: hidden;
+                border-left: 1px solid
+                  var(${getColorVarName('onBackground', 'standard')});
+                background-color: ${props.imageBackgroundColor ??
+                `var(${getGlobalColorVarName('gray6')})`};
+                position: relative;
+
+                &::before {
+                  position: absolute;
+                  z-index: 2;
+                  width: 100%;
+                  height: 100%;
+                  top: 0;
+                  left: 0;
+                  content: '';
+                  background-color: var(${getGlobalColorVarName('gray6')});
+                  ${imageWrapperBeforeInitialStyles};
+                }
+              `}
+            >
+              <div
+                css={css`
+                  position: absolute;
+                  z-index: 1;
+                  bottom: ${commonStyles.spacing.xs};
+                  right: ${commonStyles.spacing.xs};
+                  border: 1px solid var(${getGlobalColorVarName('gray8')});
+                  padding-top: ${commonStyles.spacing.xs};
+                  padding-bottom: ${commonStyles.spacing.xs};
+                  padding-left: ${commonStyles.spacing.xs};
+                  padding-right: calc(${commonStyles.spacing.xs} + 2px);
+                  background-color: var(${getGlobalColorVarName('white')});
+                  line-height: 1;
+                `}
+              >
+                <Typography
+                  variant="caption"
+                  element="span"
+                  css={css`
+                    text-transform: uppercase;
+                    font-style: italic;
+                    color: var(${getGlobalColorVarName('gray8')});
+                  `}
+                >
+                  {caption}
+                </Typography>
+              </div>
+              <div
+                className="arrow"
+                css={css`
+                  position: absolute;
+                  z-index: 2;
+                  width: 100%;
+                  height: 100%;
+                  top: 0;
+                  left: 0;
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                  ${arrowInitialStyles};
+                `}
+              >
+                <ArrowRightIcon
+                  css={css`
+                    fill: var(${getGlobalColorVarName('white')});
+                    width: 50%;
+                    height: 50%;
+                  `}
+                />
+              </div>
+              <Image
+                className="image"
+                src={props.image.lg}
+                srcSet={getImgSrcSet(props.image)}
+                alt=""
+                role="presentation"
+                css={css`
+                  width: 100%;
+                  height: 100%;
+                  object-fit: cover;
+                  ${imageInitialStyles};
+                `}
+              />
+            </div>
             <Typography
               variant="h7"
               element="h3"
@@ -103,27 +228,6 @@ export const AlbumCell: React.FC<AlbumCellProps> = props => {
             >
               {props.title}
             </Typography>
-            <div
-              css={css`
-                flex: 1;
-                overflow: hidden;
-                border-left: 1px solid
-                  var(${getColorVarName('onBackground', 'standard')});
-                background-color: ${props.imageBackgroundColor ??
-                `var(${getGlobalColorVarName('gray6')})`};
-              `}
-            >
-              <Image
-                src={props.image.lg}
-                srcSet={getImgSrcSet(props.image)}
-                alt=""
-                css={css`
-                  width: 100%;
-                  height: 100%;
-                  object-fit: cover;
-                `}
-              />
-            </div>
           </div>
         </div>
       </a>
