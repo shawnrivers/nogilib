2a25bcec1b44e376a2d1b218da1708a8069a5fa0
Shawn Rivers <kaushou@gmail.com>
2021-10-24 13:59:08 +0900

    feat: add cache-control header to next config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,17 @@ module.exports = {
     locales: ['ja', 'en', 'zh'],
     defaultLocale: 'ja',
   },
+  async headers() {
+    return [
+      {
+        source: '/images/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
