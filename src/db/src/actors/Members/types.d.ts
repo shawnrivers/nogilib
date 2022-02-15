e478303e493a8d172826530e4b8f1f9f3c750163
Shawn Rivers <kaushou@gmail.com>
2022-02-15 23:03:30 +0900

    chore(data): add fifth member key

--- src/db/src/actors/Members/types.d.ts
+++ src/db/src/actors/Members/types.d.ts
@@ -101,4 +101,12 @@ export type MemberNameKey =
   | 'kashiwayukina'
   | 'andoumikumo'
   | 'iwaseyumiko'
-  | 'kojimaharuna';
+  | 'kojimaharuna'
+  | 'inouenagi'
+  | 'ichinosemiku'
+  | 'sugawarasatsuki'
+  | 'ogawaaya'
+  | 'tomisatonao'
+  | 'okudairoha'
+  | 'nakanishiaruno'
+  | 'iokimao';
