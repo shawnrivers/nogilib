7012818662784e5a225933557eba3dfe277a36fc
Shawn <kaushou@gmail.com>
2022-04-01 20:59:56 +0900

    chore(data): fix members sorting

--- src/db/src/actors/Members/raw/editor/fifthGen.ts
+++ src/db/src/actors/Members/raw/editor/fifthGen.ts
@@ -3,125 +3,6 @@ import { createMemberRaw } from 'db/src/actors/Members/raw/creator';
 
 export const FIFTH_GEN_MEMBERS: MemberRaw[] = [
   createMemberRaw({
-    name: 'inouenagi',
-    nameNotations: ['井上', '和', 'いのうえ', 'なぎ', 'inoue', 'nagi'],
-    join: 'fifth',
-    birthday: '2005-02-17',
-    height: 156,
-    bloodType: 'b',
-    origin: '神奈川県',
-    sites: [
-      {
-        title: 'profile',
-        url: 'https://www.nogizaka46.com/s/n46/artist/55389',
-      },
-    ],
-  }),
-  createMemberRaw({
-    name: 'ichinosemiku',
-    nameNotations: ['一ノ瀬', '美空', 'いちのせ', 'みく', 'ichinose', 'miku'],
-    join: 'fifth',
-    birthday: '2003-05-24',
-    height: 156,
-    bloodType: 'b',
-    origin: '福岡県',
-    sites: [
-      {
-        title: 'profile',
-        url: 'https://www.nogizaka46.com/s/n46/artist/55390',
-      },
-    ],
-  }),
-  createMemberRaw({
-    name: 'sugawarasatsuki',
-    nameNotations: [
-      '菅原',
-      '咲月',
-      'すがわら',
-      'さつき',
-      'sugawara',
-      'satsuki',
-    ],
-    join: 'fifth',
-    birthday: '2005-10-31',
-    height: 157,
-    bloodType: 'a',
-    origin: '千葉県',
-    sites: [
-      {
-        title: 'profile',
-        url: 'https://www.nogizaka46.com/s/n46/artist/55391',
-      },
-    ],
-  }),
-  createMemberRaw({
-    name: 'ogawaaya',
-    nameNotations: ['小川', '彩', 'おがわ', 'あや', 'ogawa', 'aya'],
-    join: 'fifth',
-    birthday: '2007-06-27',
-    height: 154,
-    bloodType: 'b',
-    origin: '千葉県',
-    sites: [
-      {
-        title: 'profile',
-        url: 'https://www.nogizaka46.com/s/n46/artist/55392',
-      },
-    ],
-  }),
-  createMemberRaw({
-    name: 'tomisatonao',
-    nameNotations: ['冨里', '奈央', 'とみさと', 'なお', 'tomisato', 'nao'],
-    join: 'fifth',
-    birthday: '2006-09-18',
-    height: 164,
-    bloodType: 'o',
-    origin: '千葉県',
-    sites: [
-      {
-        title: 'profile',
-        url: 'https://www.nogizaka46.com/s/n46/artist/55393',
-      },
-    ],
-  }),
-  createMemberRaw({
-    name: 'okudairoha',
-    nameNotations: ['奥田', 'いろは', 'おくだ', 'いろは', 'okuta', 'iroha'],
-    join: 'fifth',
-    birthday: '2005-08-20',
-    height: 157,
-    bloodType: 'a',
-    origin: '千葉県',
-    sites: [
-      {
-        title: 'profile',
-        url: 'https://www.nogizaka46.com/s/n46/artist/55394',
-      },
-    ],
-  }),
-  createMemberRaw({
-    name: 'nakanishiaruno',
-    nameNotations: [
-      '中西',
-      'アルノ',
-      'なかにし',
-      'あるの',
-      'nakanishi',
-      'aruno',
-    ],
-    join: 'fifth',
-    birthday: '2003-03-17',
-    height: 157,
-    bloodType: 'a',
-    origin: '千葉県',
-    sites: [
-      {
-        title: 'profile',
-        url: 'https://www.nogizaka46.com/s/n46/artist/55395',
-      },
-    ],
-  }),
-  createMemberRaw({
     name: 'iokimao',
     nameNotations: ['五百城', '茉央', 'いおき', 'まお', 'ioki', 'mao'],
     join: 'fifth',
@@ -148,6 +29,125 @@ export const FIFTH_GEN_MEMBERS: MemberRaw[] = [
       {
         title: 'profile',
         url: 'https://www.nogizaka46.com/s/n46/artist/55397',
+      },
+    ],
+  }),
+  createMemberRaw({
+    name: 'ichinosemiku',
+    nameNotations: ['一ノ瀬', '美空', 'いちのせ', 'みく', 'ichinose', 'miku'],
+    join: 'fifth',
+    birthday: '2003-05-24',
+    height: 156,
+    bloodType: 'b',
+    origin: '福岡県',
+    sites: [
+      {
+        title: 'profile',
+        url: 'https://www.nogizaka46.com/s/n46/artist/55390',
+      },
+    ],
+  }),
+  createMemberRaw({
+    name: 'inouenagi',
+    nameNotations: ['井上', '和', 'いのうえ', 'なぎ', 'inoue', 'nagi'],
+    join: 'fifth',
+    birthday: '2005-02-17',
+    height: 156,
+    bloodType: 'b',
+    origin: '神奈川県',
+    sites: [
+      {
+        title: 'profile',
+        url: 'https://www.nogizaka46.com/s/n46/artist/55389',
+      },
+    ],
+  }),
+  createMemberRaw({
+    name: 'ogawaaya',
+    nameNotations: ['小川', '彩', 'おがわ', 'あや', 'ogawa', 'aya'],
+    join: 'fifth',
+    birthday: '2007-06-27',
+    height: 154,
+    bloodType: 'b',
+    origin: '千葉県',
+    sites: [
+      {
+        title: 'profile',
+        url: 'https://www.nogizaka46.com/s/n46/artist/55392',
+      },
+    ],
+  }),
+  createMemberRaw({
+    name: 'okudairoha',
+    nameNotations: ['奥田', 'いろは', 'おくだ', 'いろは', 'okuta', 'iroha'],
+    join: 'fifth',
+    birthday: '2005-08-20',
+    height: 157,
+    bloodType: 'a',
+    origin: '千葉県',
+    sites: [
+      {
+        title: 'profile',
+        url: 'https://www.nogizaka46.com/s/n46/artist/55394',
+      },
+    ],
+  }),
+  createMemberRaw({
+    name: 'sugawarasatsuki',
+    nameNotations: [
+      '菅原',
+      '咲月',
+      'すがわら',
+      'さつき',
+      'sugawara',
+      'satsuki',
+    ],
+    join: 'fifth',
+    birthday: '2005-10-31',
+    height: 157,
+    bloodType: 'a',
+    origin: '千葉県',
+    sites: [
+      {
+        title: 'profile',
+        url: 'https://www.nogizaka46.com/s/n46/artist/55391',
+      },
+    ],
+  }),
+  createMemberRaw({
+    name: 'tomisatonao',
+    nameNotations: ['冨里', '奈央', 'とみさと', 'なお', 'tomisato', 'nao'],
+    join: 'fifth',
+    birthday: '2006-09-18',
+    height: 164,
+    bloodType: 'o',
+    origin: '千葉県',
+    sites: [
+      {
+        title: 'profile',
+        url: 'https://www.nogizaka46.com/s/n46/artist/55393',
+      },
+    ],
+  }),
+  createMemberRaw({
+    name: 'nakanishiaruno',
+    nameNotations: [
+      '中西',
+      'アルノ',
+      'なかにし',
+      'あるの',
+      'nakanishi',
+      'aruno',
+    ],
+    join: 'fifth',
+    birthday: '2003-03-17',
+    height: 157,
+    bloodType: 'a',
+    origin: '千葉県',
+    sites: [
+      {
+        title: 'profile',
+        url: 'https://www.nogizaka46.com/s/n46/artist/55395',
       },
     ],
   }),
