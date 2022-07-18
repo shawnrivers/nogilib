4695f839ba418f55d683dc003dabd381bc593676
Shawn <kaushou@gmail.com>
2022-07-18 11:25:32 +0900

    chore(data): add 30th single formation

---
+++ src/db/src/actors/Discography/raw/editor/singles/30.ts
@@ -0,0 +1,9 @@
+import { createSingleRaw } from '../../creators';
+
+export const THIRTIETH_SINGLE = createSingleRaw({
+  title: 'タイトル未定',
+  number: '30',
+  release: '2022-08-30',
+  cdTypes: ['A'],
+  songs: [{ title: 'タイトル未定', inCdType: ['A'] }],
+});
---
+++ src/db/src/actors/Songs/raw/editor/singles/30.ts
@@ -0,0 +1,37 @@
+import { createSongRaw } from '../../creator';
+
+export const THIRTIETH_SINGLE_SONGS = [
+  createSongRaw({
+    title: 'タイトル未定',
+    type: 'title',
+    center: ['kakiharuka'],
+    fukujin: 'row-1-2',
+    formations: {
+      firstRow: [
+        'yodayuuki',
+        'saitouasuka',
+        'kakiharuka',
+        'yamashitamizuki',
+        'endousakura',
+      ],
+      secondRow: [
+        'tamuramayu',
+        'kuboshiori',
+        'umezawaminami',
+        'akimotomanatsu',
+        'iwamotorenka',
+        'tsutsuiayame',
+      ],
+      thirdRow: [
+        'kanagawasaya',
+        'seimiyarei',
+        'kakehashisayaka',
+        'suzukiayane',
+        'higuchihina',
+        'shibatayuna',
+        'satoukaede',
+        'yumikinao',
+      ],
+    },
+  }),
+];
