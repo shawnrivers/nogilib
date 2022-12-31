e941218ce99d4c86756ce57836b60cfccee2a842
Shawn <kaushou@gmail.com>
2022-12-31 21:20:33 +0900

    chore(data): update member profile images

--- scripts/src/downloadImages/index.ts
+++ scripts/src/downloadImages/index.ts
@@ -2,7 +2,7 @@ import { writeFile } from 'fs/promises';
 import axios from 'axios';
 
 const getMembersData = async () => {
-  return (await import('../../../src/data/members.json')).default;
+  return (await import('../../../src/db/result/member.json')).default;
 };
 
 const downloadImage = async (url: string, savePath: string) => {
